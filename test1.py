e8a03feefe5949bdfd23f4d7d19e976f8f52c832
chaitanya200e <chaitanyamandale125@gmail.com>
2026-08-20 19:17:51 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,10 @@
+class chaitanya:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return f"Hello, {self.name}!"
+
+
+obj = chaitanya("Chaitanya")
+print(obj.greet())
